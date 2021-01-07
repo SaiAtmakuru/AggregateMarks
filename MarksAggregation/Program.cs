eb067fd8bf5bc519799d6fb30eeedf871f894ac6
Atmakuru <Sai.Atmakuru@cdk.com>
2021-01-07 17:55:13 +0530

    Initial commit

---
+++ MarksAggregation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MarksAggregation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Test1 - with 3 students and 5 subjects 
+            StudentMarks test1 = new StudentMarks(3, 5, new int[,] { { 75, 76, 65, 87, 87 }, { 78, 76, 68, 56, 89 }, { 67, 87, 78, 77, 65 } });
+            int minSubjectIndex = test1.FindAverageSubjectIndex();
+            int[] finalAggrgateMarks = test1.TotalMarksExcludingSubject(minSubjectIndex);
+            Array.Sort(finalAggrgateMarks);
+            Console.WriteLine(string.Join(" , ", finalAggrgateMarks));
+
+            // Test2 - When students and subject count dosent match array length  
+            // Output - says with message "Enter correct array with specified students and subjects"
+            StudentMarks test2 = new StudentMarks(3, 4, new int[,] { { 75, 76, 65, 87, 87 }, { 78, 76, 68, 56, 89 }, { 67, 87, 78, 77, 65 } });
+
+            //Test3 - with 5 students and 6 subjects 
+            StudentMarks test3 = new StudentMarks(5, 6, new int[,] { { 75, 76, 65, 87, 87,99}, { 78, 76, 68, 56, 89 ,95 }, { 67, 87, 78, 77, 65,35 }, { 67, 87, 34, 77, 65, 35 }, { 67, 87, 78, 36, 65, 35 } });
+            minSubjectIndex = test3.FindAverageSubjectIndex();
+            finalAggrgateMarks = test3.TotalMarksExcludingSubject(minSubjectIndex);
+            Array.Sort(finalAggrgateMarks);
+            Console.WriteLine(string.Join(" , ", finalAggrgateMarks));
+        }
+    }
+}
---
+++ MarksAggregation/StudentMarks.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MarksAggregation
+{
+    class StudentMarks
+    {
+        int _numOfStudents;
+        int _numOfSubjects;
+        int[,] marks;
+        public StudentMarks(int numOfStudents , int numOfSubjects, int[,] studentMarks)
+        {
+            if (numOfStudents != studentMarks.GetLength(0) || numOfSubjects != studentMarks.GetLength(1))
+            {
+                Console.WriteLine("Enter correct array with specified students and subjects");
+            }
+            else
+            {
+                _numOfStudents = numOfStudents;
+                _numOfSubjects = numOfSubjects;
+                marks = studentMarks;
+            }
+
+        }
+
+        public int FindAverageSubjectIndex()
+        {
+            Dictionary<int, double> marksAverage = new Dictionary<int, double>();
+            for (int i = 0; i < _numOfSubjects; i++)
+            {
+                List<int> subAverage = new List<int>();
+                for (int j = 0; j < _numOfStudents; j++)
+                {
+                    subAverage.Add(marks[j, i]);
+                }
+                marksAverage.Add(i, subAverage.Average());
+            }
+            return marksAverage.OrderBy(t => t.Value).ToDictionary(t => t.Key, t => t.Value).First().Key;
+        }
+
+        public int[] TotalMarksExcludingSubject(int minSubjectIndex)
+        {
+            int[] resultMarks = new int[_numOfStudents];
+            for (int i = 0; i < _numOfStudents; i++)
+            {
+                int totalMarks = 0;
+                for (int j = 0; j < _numOfSubjects; j++)
+                {
+                    if (j != minSubjectIndex)
+                        totalMarks += marks[i, j];
+                }
+                resultMarks[i] = totalMarks;
+            }
+            return resultMarks;
+        }
+    }
+}
